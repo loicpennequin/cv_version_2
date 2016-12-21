aa00339f99c72cc410f5ad73365be3c807a32d53
Loic Pennequin <lo.pennequin@gmail.com>
2016-12-21 18:24:29 +0100

    fin journée 21-12-2016 CE CV A DE LA GUEULE PUTAIN

--- js/javascript.js
+++ js/javascript.js
@@ -4,26 +4,24 @@ $(document).ready(function(){
 	 		$("#section1").toggle("slow","swing");
 			$("#btn1").toggleClass("opacity-on");
 			$("#btn1").toggleClass("white-shadow");	
-			
-	     /*$("#btn1").fadeTo(500,1); */
-	      });
+	 });
 	 
 	 $("#btn2").click(function(){
 	     $("#section2").toggle("slow","swing");
 	     $("#btn2").toggleClass("opacity-on");	
-	     			$("#btn2").toggleClass("white-shadow");	
+	     $("#btn2").toggleClass("white-shadow");	
 	 });
 	 
 	 $("#btn3").click(function(){
 	     $("#section3").toggle("slow","swing");
 	     $("#btn3").toggleClass("opacity-on");	
-	     			$("#btn3").toggleClass("white-shadow");	
+	     $("#btn3").toggleClass("white-shadow");	
 	 });
 	 
 	 $("#btn4").click(function(){
 	     $("#section4").toggle("slow","swing");
 	     $("#btn4").toggleClass("opacity-on");	
-	     			$("#btn4").toggleClass("white-shadow");	
+	     $("#btn4").toggleClass("white-shadow");	
 	 });
 	 
     
