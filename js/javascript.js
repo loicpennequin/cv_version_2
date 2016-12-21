d67592f25d41c6677bf2d5de6d56330d7827512a
Loic Pennequin <lo.pennequin@gmail.com>
2016-12-21 15:42:47 +0100

    continuation js

--- js/javascript.js
+++ js/javascript.js
@@ -2,17 +2,21 @@ $(document).ready(function(){
    
     $("#btn1").click(function(){
         $("#section1").toggle("slow","swing");
-    });
+        $("#btn1").fadeTo(500,1);
+         });
     
     $("#btn2").click(function(){
         $("#section2").toggle("slow","swing");
+        $("#btn2").fadeTo(500,1);
     });
     
     $("#btn3").click(function(){
         $("#section3").toggle("slow","swing");
+        $("#btn3").fadeTo(500,1);
     });
     
     $("#btn4").click(function(){
         $("#section4").toggle("slow","swing");
+        $("#btn4").fadeTo(500,1);
     });
 });
