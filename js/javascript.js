f8e30da06be6cffd0018694715dc5d0067ebda4e
Loic Pennequin <lo.pennequin@gmail.com>
2016-12-21 15:11:07 +0100

    Refonte du projet pour incorporer du jquery.

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+   
+    $("#btn1").click(function(){
+        $("#section1").toggle("slow","swing");
+    });
+    
+    $("#btn2").click(function(){
+        $("#section2").toggle("slow","swing");
+    });
+    
+    $("#btn3").click(function(){
+        $("#section3").toggle("slow","swing");
+    });
+    
+    $("#btn4").click(function(){
+        $("#section4").toggle("slow","swing");
+    });
+});
