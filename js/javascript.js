6df86e939d085825738879dca0d943fde8ecf76c
Loic Pennequin <lo.pennequin@gmail.com>
2016-12-21 17:38:58 +0100

    on teste des trucs, des trucs et encore des trucs

--- js/javascript.js
+++ js/javascript.js
@@ -1,22 +1,30 @@
 $(document).ready(function(){
    
-    $("#btn1").click(function(){
-        $("#section1").toggle("slow","swing");
-        $("#btn1").fadeTo(500,1);
-         });
+	 $("#btn1").click(function(){
+	 		$("#section1").toggle("slow","swing");
+			$("#btn1").toggleClass("opacity-on");
+			$("#btn1").toggleClass("white-shadow");	
+			
+	     /*$("#btn1").fadeTo(500,1); */
+	      });
+	 
+	 $("#btn2").click(function(){
+	     $("#section2").toggle("slow","swing");
+	     $("#btn2").toggleClass("opacity-on");	
+	     			$("#btn2").toggleClass("white-shadow");	
+	 });
+	 
+	 $("#btn3").click(function(){
+	     $("#section3").toggle("slow","swing");
+	     $("#btn3").toggleClass("opacity-on");	
+	     			$("#btn3").toggleClass("white-shadow");	
+	 });
+	 
+	 $("#btn4").click(function(){
+	     $("#section4").toggle("slow","swing");
+	     $("#btn4").toggleClass("opacity-on");	
+	     			$("#btn4").toggleClass("white-shadow");	
+	 });
+	 
     
-    $("#btn2").click(function(){
-        $("#section2").toggle("slow","swing");
-        $("#btn2").fadeTo(500,1);
-    });
-    
-    $("#btn3").click(function(){
-        $("#section3").toggle("slow","swing");
-        $("#btn3").fadeTo(500,1);
-    });
-    
-    $("#btn4").click(function(){
-        $("#section4").toggle("slow","swing");
-        $("#btn4").fadeTo(500,1);
-    });
 });
