f20ddfae3f145e59a8bb0708ebc2eef01988387c
Loic Pennequin <lo.pennequin@gmail.com>
2016-12-22 00:39:42 +0100

    modifications javascript et rotation du backgrund jumbotron en xs

--- js/javascript.js
+++ js/javascript.js
@@ -2,27 +2,35 @@ $(document).ready(function(){
    
 	 $("#btn1").click(function(){
 	 		$("#section1").toggle("slow","swing");
-			$("#btn1").toggleClass("opacity-on");
-			$("#btn1").toggleClass("white-shadow");	
+	 		$("#section2, #section3, #section4").hide("slow");
+			$("#btn1").toggleClass("button-pressed");
+			$("#btn2, #btn3, #btn4").removeClass("button-pressed");	
 	 });
 	 
-	 $("#btn2").click(function(){
-	     $("#section2").toggle("slow","swing");
-	     $("#btn2").toggleClass("opacity-on");	
-	     $("#btn2").toggleClass("white-shadow");	
+	  $("#btn2").click(function(){
+	 		$("#section2").toggle("slow","swing");
+			$("#section1, #section3, #section4").hide("slow");
+			$("#btn2").toggleClass("button-pressed");
+			$("#btn1, #btn3, #btn4").removeClass("button-pressed");		
 	 });
 	 
-	 $("#btn3").click(function(){
-	     $("#section3").toggle("slow","swing");
-	     $("#btn3").toggleClass("opacity-on");	
-	     $("#btn3").toggleClass("white-shadow");	
+	  $("#btn3").click(function(){
+	 		$("#section3").toggle("slow","swing");
+			$("#section1, #section2, #section4").hide("slow")
+			$("#btn3").toggleClass("button-pressed");
+			$("#btn1, #btn2, #btn4").removeClass("button-pressed");	
 	 });
 	 
-	 $("#btn4").click(function(){
-	     $("#section4").toggle("slow","swing");
-	     $("#btn4").toggleClass("opacity-on");	
-	     $("#btn4").toggleClass("white-shadow");	
+	  $("#btn4").click(function(){
+	 		$("#section4").toggle("slow","swing");
+	 		$("#section1, #section2, #section3").hide("slow");
+			$("#btn4").toggleClass("button-pressed");
+			$("#btn1, #btn2, #btn3").removeClass("button-pressed");
+			
 	 });
 	 
-    
 });
+	 
+	
+	 
+
