b3ac6a7aa5543ec58a7b7240e647fad0258c0399
Loic Pennequin <lo.pennequin@gmail.com>
2016-12-22 18:12:02 +0100

    implementation de sass + modifs mineures

--- js/javascript.js
+++ js/javascript.js
@@ -1,27 +1,27 @@
 $(document).ready(function(){
    
-	 $("#btn1").click(function(){
+	 $("#btn1").on("click", function(){
 	 		$("#section1").toggle("slow","swing");
 	 		$("#section2, #section3, #section4").hide("slow");
 			$("#btn1").toggleClass("button-pressed");
 			$("#btn2, #btn3, #btn4").removeClass("button-pressed");	
 	 });
 	 
-	  $("#btn2").click(function(){
+	  $("#btn2").on("click", function(){
 	 		$("#section2").toggle("slow","swing");
 			$("#section1, #section3, #section4").hide("slow");
 			$("#btn2").toggleClass("button-pressed");
 			$("#btn1, #btn3, #btn4").removeClass("button-pressed");		
 	 });
 	 
-	  $("#btn3").click(function(){
+	  $("#btn3").on("click", function(){
 	 		$("#section3").toggle("slow","swing");
 			$("#section1, #section2, #section4").hide("slow")
 			$("#btn3").toggleClass("button-pressed");
 			$("#btn1, #btn2, #btn4").removeClass("button-pressed");	
 	 });
 	 
-	  $("#btn4").click(function(){
+	  $("#btn4").on("click", function(){
 	 		$("#section4").toggle("slow","swing");
 	 		$("#section1, #section2, #section3").hide("slow");
 			$("#btn4").toggleClass("button-pressed");
